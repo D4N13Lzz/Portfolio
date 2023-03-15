0b289262544964415fe9166be3831932558a0db4
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-03-15 01:06:02 -0300

    asd

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,18 +1,35 @@
-const home = document.querySelector('.home')
-const about = document.querySelector('.about')
-const project = document.querySelector('.project')
-const contact = document.querySelector('.contact')
-const footer = document.querySelector('.footer')
-const scrollDark = document.querySelector('.scrollDark')
-const navbar = document.querySelector('.navbar')
+const home = document.querySelector('.home');
+const about = document.querySelector('.about');
+const project = document.querySelector('.project');
+const contact = document.querySelector('.contact');
+const footer = document.querySelector('.footer');
+const scrollDark = document.querySelector('.scrollDark');
+const navbar = document.querySelector('.navbar');
 
-const iconMode = document.querySelector('.iconMode')
-    iconMode.addEventListener('click', () => {
-    home.classList.toggle('dark')
-    about.classList.toggle('dark')
-    project.classList.toggle('dark')
-    contact.classList.toggle('dark')
-    footer.classList.toggle('dark')
-    scrollDark.classList.toggle('dark')
-    navbar.classList.toggle('dark')
-})
+const iconMode = document.querySelector('.iconMode');
+
+let modeText = iconMode.querySelector('.mode-text');
+
+if (!modeText) {
+  modeText = document.createElement('span');
+  modeText.classList.add('mode-text');
+  modeText.textContent = 'Dark Mode';
+  iconMode.appendChild(modeText);
+}
+
+iconMode.addEventListener('click', () => {
+  home.classList.toggle('dark');
+  about.classList.toggle('dark');
+  project.classList.toggle('dark');
+  contact.classList.toggle('dark');
+  footer.classList.toggle('dark');
+  scrollDark.classList.toggle('dark');
+  navbar.classList.toggle('dark');
+  iconMode.classList.toggle('dark');
+
+  if (iconMode.classList.contains('dark')) {
+    modeText.textContent = 'Light Mode';
+  } else {
+    modeText.textContent = 'Dark Mode';
+  }
+});
