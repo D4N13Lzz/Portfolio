e674c4fe90814d4ddb025efcc2a962bfb923c1a1
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-03-15 11:43:59 -0300

    Update darkmode.js

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -17,19 +17,56 @@ if (!modeText) {
   iconMode.appendChild(modeText);
 }
 
-iconMode.addEventListener('click', () => {
-  home.classList.toggle('dark');
-  about.classList.toggle('dark');
-  project.classList.toggle('dark');
-  contact.classList.toggle('dark');
-  footer.classList.toggle('dark');
-  scrollDark.classList.toggle('dark');
-  navbar.classList.toggle('dark');
-  iconMode.classList.toggle('dark');
+// Obtém o valor armazenado no localStorage
+const storedTheme = localStorage.getItem('theme');
 
-  if (iconMode.classList.contains('dark')) {
+// Define o tema padrão como "light"
+let currentTheme = 'light';
+
+// Se houver um valor armazenado no localStorage, usa esse valor como tema atual
+if (storedTheme) {
+  currentTheme = storedTheme;
+}
+
+// Define o tema atual na página
+updateTheme(currentTheme);
+
+iconMode.addEventListener('click', () => {
+  // Alterna entre "light" e "dark"
+  currentTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+  // Atualiza o valor armazenado no localStorage
+  localStorage.setItem('theme', currentTheme);
+
+  // Define o tema atual na página
+  updateTheme(currentTheme);
+  
+  // Atualiza o texto do botão de acordo com o tema atual
+  if (currentTheme === 'dark') {
     modeText.textContent = 'Light Mode';
   } else {
     modeText.textContent = 'Dark Mode';
   }
 });
+
+function updateTheme(theme) {
+  if (theme === 'dark') {
+    home.classList.add('dark');
+    about.classList.add('dark');
+    project.classList.add('dark');
+    contact.classList.add('dark');
+    footer.classList.add('dark');
+    scrollDark.classList.add('dark');
+    navbar.classList.add('dark');
+    iconMode.classList.add('dark');
+  } else {
+    home.classList.remove('dark');
+    about.classList.remove('dark');
+    project.classList.remove('dark');
+    contact.classList.remove('dark');
+    footer.classList.remove('dark');
+    scrollDark.classList.remove('dark');
+    navbar.classList.remove('dark');
+    iconMode.classList.remove('dark');
+  }
+}
