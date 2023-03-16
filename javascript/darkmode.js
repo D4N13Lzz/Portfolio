6e71d259da502b1922e893657981da7d062fbb6a
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-03-16 04:02:13 -0300

    UI

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -7,45 +7,36 @@ const scrollDark = document.querySelector('.scrollDark');
 const navbar = document.querySelector('.navbar');
 
 const iconMode = document.querySelector('.iconMode');
-
-let modeText = iconMode.querySelector('.mode-text');
-
-if (!modeText) {
-  modeText = document.createElement('span');
-  modeText.classList.add('mode-text');
-  modeText.textContent = 'Dark Mode';
-  iconMode.appendChild(modeText);
-}
+const moonIcon = iconMode.querySelector('.moon-icon');
+const sunIcon = iconMode.querySelector('.sun-icon');
+const darkModeText = iconMode.querySelector('.dark-mode-text');
+const lightModeText = iconMode.querySelector('.light-mode-text');
 
 // Obtém o valor armazenado no localStorage
 const storedTheme = localStorage.getItem('theme');
-
-// Define o tema padrão como "light"
-let currentTheme = 'light';
-
-// Se houver um valor armazenado no localStorage, usa esse valor como tema atual
+let currentTheme = localStorage.getItem('theme') || 'light';
 if (storedTheme) {
   currentTheme = storedTheme;
 }
-
-// Define o tema atual na página
 updateTheme(currentTheme);
 
 iconMode.addEventListener('click', () => {
-  // Alterna entre "light" e "dark"
   currentTheme = currentTheme === 'light' ? 'dark' : 'light';
-
-  // Atualiza o valor armazenado no localStorage
   localStorage.setItem('theme', currentTheme);
-
   // Define o tema atual na página
   updateTheme(currentTheme);
-  
+
   // Atualiza o texto do botão de acordo com o tema atual
   if (currentTheme === 'dark') {
-    modeText.textContent = 'Light Mode';
+    iconMode.classList.remove('light');
+    iconMode.classList.add('dark');
+    darkModeText.style.display = 'none';
+    lightModeText.style.display = 'inline';
   } else {
-    modeText.textContent = 'Dark Mode';
+    iconMode.classList.remove('dark');
+    iconMode.classList.add('light');
+    darkModeText.style.display = 'inline';
+    lightModeText.style.display = 'none';
   }
 });
 
@@ -58,7 +49,12 @@ function updateTheme(theme) {
     footer.classList.add('dark');
     scrollDark.classList.add('dark');
     navbar.classList.add('dark');
+
     iconMode.classList.add('dark');
+    moonIcon.style.display = 'none';
+    sunIcon.style.display = 'inline';
+    darkModeText.style.display = 'none';
+    lightModeText.style.display = 'inline';
   } else {
     home.classList.remove('dark');
     about.classList.remove('dark');
@@ -67,7 +63,36 @@ function updateTheme(theme) {
     footer.classList.remove('dark');
     scrollDark.classList.remove('dark');
     navbar.classList.remove('dark');
+
     iconMode.classList.remove('dark');
+    moonIcon.style.display = 'inline';
+    sunIcon.style.display = 'none';
+    darkModeText.style.display = 'inline';
+    lightModeText.style.display = 'none';
   }
 }
-// DarkMode com autosave
+
+window.addEventListener('resize', () => {
+  const iconMode = document.querySelector('.iconMode');
+  const iconWidth = iconMode.offsetWidth;
+  const iconHeight = iconMode.offsetHeight;
+  const iconTop = iconMode.offsetTop;
+  const iconLeft = iconMode.offsetLeft;
+
+  const darkModeText = iconMode.querySelector('.dark-mode-text');
+  const lightModeText = iconMode.querySelector('.light-mode-text');
+  const textWidth = darkModeText.offsetWidth;
+  const textHeight = darkModeText.offsetHeight;
+  const textTop = iconTop + (iconHeight - textHeight) / 2 + 'px';
+  const textLeft = iconLeft + iconWidth + 10 + 'px';
+
+  darkModeText.style.top = textTop;
+  darkModeText.style.left = textLeft;
+  lightModeText.style.top = textTop;
+  lightModeText.style.left = textLeft;
+});
+
+
+
+
+
